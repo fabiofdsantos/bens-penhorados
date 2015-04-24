5f2f1172188f1ffa2bc94d8372c57534bb375bc4
Fábio Santos <ffsantos92@gmail.com>
2015-04-24 23:05:34 +0100

    Fix issue with nth-childs

--- app/Jobs/Extract/RawDataExtended.php
+++ app/Jobs/Extract/RawDataExtended.php
@@ -82,57 +82,58 @@ class RawDataExtended extends Job
                     }
                 }
             }
-            $footerDetails = $crawler->filter('#dataTable > tr:nth-child(3) > td:nth-child(1) > table:nth-child(1) > tr:nth-child(1) > th:nth-child(1)');
-            for ($i = 1; $i <= ($footerDetails->filter('span')->count()+5); $i++) {
-                if ($footerDetails->filter('span:nth-child('.$i.')')->count()) {
-                    $currentSpan = trim($footerDetails->filter('span:nth-child('.$i.')')->text());
+            $footerDetails = $crawler->filter('#dataTable > tr:nth-child(3) > td:nth-child(1) > table:nth-child(1) > tr:nth-child(1)');
+            for ($i = 1; $i <= ($footerDetails->filter('th:nth-child(1) > span')->count()+6); $i++) {
+                if ($footerDetails->filter('th:nth-child(1) > span:nth-child('.$i.')')->count()) {
+                    $currentSpan = trim($footerDetails->filter('th:nth-child(1) > span:nth-child('.$i.')')->text());
                     if (preg_match('/Caracter/i', $currentSpan, $match)) {
                         try {
-                            $data->desc = trim($footerDetails->filter('span:nth-child('.($i+1).')')->text());
+                            $data->desc = trim($footerDetails->filter('th:nth-child(1) > span:nth-child('.($i+1).')')->text());
                         } catch (\Exception $e) {
                             $data->desc = null;
                         }
                     } elseif (preg_match('/Nome dos Executados/i', $currentSpan, $match)) {
                         try {
-                            $data->owners = trim($footerDetails->filter('span:nth-child('.($i+1).')')->text());
+                            $data->owners = trim($footerDetails->filter('th:nth-child(1) > span:nth-child('.($i+1).')')->text());
                         } catch (\Exception $e) {
                             $data->owners = null;
                         }
                     } elseif (preg_match('/Fiel Deposit/i', $currentSpan, $match)) {
                         try {
-                            $data->depositary = trim($footerDetails->filter('span:nth-child('.($i+1).')')->text());
+                            $data->depositary = trim($footerDetails->filter('th:nth-child(1) > span:nth-child('.($i+1).')')->text());
                             $data->depositary = preg_replace('/(\\n)|(\\t)/', '', $data->depositary);
                         } catch (\Exception $e) {
                             $data->depositary = null;
                         }
                     } elseif (preg_match('/Mediador/i', $currentSpan, $match)) {
                         try {
-                            $data->mediator = trim($footerDetails->filter('span:nth-child('.($i+1).')')->text());
+                            $data->mediator = trim($footerDetails->filter('th:nth-child(1) > span:nth-child('.($i+1).')')->text());
                             $data->mediator = preg_replace('/(\\n)|(\\t)/', '', $data->mediator);
                         } catch (\Exception $e) {
                             $data->mediator = null;
                         }
                     } elseif (preg_match('/examinar o bem/i', $currentSpan, $match)) {
                         try {
-                            $data->preview = trim($footerDetails->filter('span:nth-child('.($i+1).')')->text());
+                            $data->preview = trim($footerDetails->filter('th:nth-child(1) > span:nth-child('.($i+1).')')->text());
                         } catch (\Exception $e) {
                             $data->preview = null;
                         }
                     } elseif (preg_match('/aceitaçao das propostas/i', $currentSpan, $match)) {
                         try {
-                            $data->acceptance = trim($footerDetails->filter('span:nth-child('.($i+1).')')->text());
+                            $data->acceptance = trim($footerDetails->filter('th:nth-child(1) > span:nth-child('.($i+1).')')->text());
                         } catch (\Exception $e) {
                             $data->acceptance = null;
                         }
                     } elseif (preg_match('/abertura das propostas/i', $currentSpan, $match)) {
                         try {
-                            $data->opening = trim($footerDetails->filter('span:nth-child('.($i+1).')')->text());
+                            $data->opening = trim($footerDetails->filter('th:nth-child(1) > span:nth-child('.($i+1).')')->text());
                         } catch (\Exception $e) {
                             $data->opening = null;
                         }
                     }
                 }
             }
+            dd('x');
             $data->lat = $this->lat;
             $data->lng = $this->lng;
             $headerImages = $crawler->filter('#trFotoP > th:nth-child(2)');
