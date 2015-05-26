a6c6e2b80d8079a40c4ec3b1cd336a6d897263d5
ffsantos92 <ffsantos92@gmail.com>
2015-05-26 15:22:39 +0100

    Add sign up view

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,28 @@
 var app = angular.module('bens-penhorados', [
-  'ngRoute'
+    'ngRoute'
 ]);
 
 app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'partials/home.html',
-      controller: 'HomeCtrl'
-    })
-    .when('/login', {
-      templateUrl: 'partials/login.html',
-      controller: 'LoginCtrl'
-    })
-    .otherwise('/404', {
-      templateUrl: 'partials/404.html'
-    });
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'partials/login.html',
+            controller: 'LoginCtrl'
+        })
+        .when('/register', {
+            templateUrl: 'partials/register.html'
+        })
+        .otherwise('/404', {
+            templateUrl: 'partials/404.html'
+        });
 }]);
 
 app.controller('HomeCtrl', function($scope, $http) {
-    $http.get('../api/v1/home'
-).then(function(response) {
-    $scope.newItems = response.data.newItems;
-    $scope.endingSoon = response.data.endingSoon;
-  });
+    $http.get('../api/v1/home').then(function(response) {
+        $scope.newItems = response.data.newItems;
+        $scope.endingSoon = response.data.endingSoon;
+    });
 });
