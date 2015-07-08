5424769b4962b97f682a78735d0be40adeff8cf3
ffsantos92 <ffsantos92@gmail.com>
2015-07-08 14:44:30 +0100

    Add vehicles_makes_models table

---
+++ database/migrations/2015_07_08_134252_create_vehicles_makes_models_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesMakesModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('vehicles_makes_models', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('parent_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        if (Schema::hasTable('vehicles_makes_models')) {
+            Schema::drop('vehicles_makes_models');
+        }
+    }
+}
