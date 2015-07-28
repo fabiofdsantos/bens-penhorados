356cf414a9e30eb702f6ab021b5d4de815b24078
ffsantos92 <ffsantos92@gmail.com>
2015-07-29 00:29:29 +0100

    Add the item category model

---
+++ app/Models/Items/Attributes/ItemCategory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Items\Attributes;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * This is the item's category model class.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property int $code
+ */
+class VehicleCategory extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'item_categories';
+
+    /**
+     * The primary key column.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+}
