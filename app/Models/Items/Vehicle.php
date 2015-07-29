f7416862e294cc052e5bced3b943787b132d98d3
ffsantos92 <ffsantos92@gmail.com>
2015-07-29 01:17:38 +0100

    Include a header comment in some files

--- app/Models/Items/Vehicle.php
+++ app/Models/Items/Vehicle.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Bens Penhorados, an undergraduate capstone project.
+ *
+ * (c) Fábio Santos <ffsantos92@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Models\Items;
 
 use App\Models\Items\Attributes\VehicleCategory;
