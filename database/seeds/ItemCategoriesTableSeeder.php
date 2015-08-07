54a6e56993b980fff6f5b310297a8a8071643202
ffsantos92 <ffsantos92@gmail.com>
2015-08-07 21:35:38 +0100

    Add a new category: Estabelecimentos comerciais

--- database/seeds/ItemCategoriesTableSeeder.php
+++ database/seeds/ItemCategoriesTableSeeder.php
@@ -20,7 +20,8 @@ class ItemCategoriesTableSeeder extends Seeder
         $categories = [
             ['name' => 'Veículos', 'slug' => 'veiculos', 'code' => '01'],
             ['name' => 'Imóveis', 'slug' => 'imoveis', 'code' => '02'],
-            ['name' => 'Participações sociais', 'slug' => 'part-sociais', 'code' => '05'],
+            ['name' => 'Participações sociais', 'slug' => 'participacoes-sociais', 'code' => '05'],
+            ['name' => 'Estabelecimentos comerciais', 'estabelecimentos-comerciais', 'code' => '08'],
             ['name' => 'Outros', 'slug' => 'outros', 'code' => '09'],
         ];
 
