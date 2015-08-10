bcca1906177d918bc20d8c7d285ac89481b51d03
ffsantos92 <ffsantos92@gmail.com>
2015-08-10 21:22:08 +0100

    Set custom page title; Remove font awesome; Add filters.js

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
+<html ng-app="bens-penhorados">
 
 <head>
     <base href="/" />
-    <title>Bens Penhorados</title>
+    <title ng-bind="pageTitle ? pageTitle + ' - Bens Penhorados' : 'Bens Penhorados'"></title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/animate.css">
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="css/loading-bar.css">
 </head>
 
-<body ng-app="bens-penhorados">
+<body>
     <!--[if lt IE 7]>
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
@@ -42,6 +42,7 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.13.0/ui-bootstrap-tpls.min.js"></script>
 
     <script src="js/main.js"></script>
+    <script src="js/filters.js"></script>
 
     <!-- Controllers -->
     <script src="js/controllers/VehicleController.js"></script>
