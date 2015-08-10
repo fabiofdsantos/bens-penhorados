894f5cee06f47596c30a0eb85dad3ccffd563ae1
ffsantos92 <ffsantos92@gmail.com>
2015-08-10 11:24:39 +0100

    Add the property extractor wrapper

---
+++ app/Extractors/PropertyExtractorWrapper.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of Bens Penhorados, an undergraduate capstone project.
+ *
+ * (c) Fábio Santos <ffsantos92@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Extractors;
+
+/**
+ * This is the property extractor wrapper.
+ *
+ * @author Fábio Santos <ffsantos92@gmail.com>
+ */
+class PropertyExtractorWrapper extends AbstractExtractorWrapper
+{
+    const EXTRACTORS = [];
+}
