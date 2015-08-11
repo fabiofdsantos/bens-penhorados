172e471a346ee77683b942080e7b8699694bfed4
ffsantos92 <ffsantos92@gmail.com>
2015-08-11 13:17:46 +0100

    Use different values to understand which assertion fails

--- tests/Extractors/Vehicle/YearExtractorTest.php
+++ tests/Extractors/Vehicle/YearExtractorTest.php
@@ -18,20 +18,20 @@ class YearExtractorTest extends AbstractVehicleExtractorTest
 {
     public function testExtractValidYear()
     {
-        $text = 'ano: 2014';
-        $this->assertEquals(2014, $this->extractor->year($text));
+        $text = 'ano: 2009';
+        $this->assertEquals(2009, $this->extractor->year($text));
 
-        $text = 'ano 2014';
-        $this->assertEquals(2014, $this->extractor->year($text));
+        $text = 'ano 2010';
+        $this->assertEquals(2010, $this->extractor->year($text));
 
-        $text = 'de: 2014';
-        $this->assertEquals(2014, $this->extractor->year($text));
+        $text = 'de: 2011';
+        $this->assertEquals(2011, $this->extractor->year($text));
 
-        $text = 'de 2014';
-        $this->assertEquals(2014, $this->extractor->year($text));
+        $text = 'de 2012';
+        $this->assertEquals(2012, $this->extractor->year($text));
 
-        $text = 'de 01-02-2014';
-        $this->assertEquals(2014, $this->extractor->year($text));
+        $text = 'de 01-02-2013';
+        $this->assertEquals(2013, $this->extractor->year($text));
 
         $text = 'de 01/02/2014';
         $this->assertEquals(2014, $this->extractor->year($text));
