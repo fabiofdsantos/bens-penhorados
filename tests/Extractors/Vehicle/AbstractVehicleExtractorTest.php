04421db913af984513c76b236e1c86c2bec48184
ffsantos92 <ffsantos92@gmail.com>
2015-08-11 12:55:02 +0100

    Add abstract vehicle extractor test

---
+++ tests/Extractors/Vehicle/AbstractVehicleExtractorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of Bens Penhorados, an undergraduate capstone project.
+ *
+ * (c) Fábio Santos <ffsantos92@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use App\Extractors\VehicleExtractorWrapper;
+
+/**
+ * This is the abstract vehicle extractor test class.
+ *
+ * @author Fábio Santos <ffsantos92@gmail.com>
+ */
+abstract class AbstractVehicleExtractorTest extends AbstractTestCase
+{
+    protected $extractor;
+
+    public function __construct()
+    {
+        $this->extractor = new VehicleExtractorWrapper();
+    }
+}
