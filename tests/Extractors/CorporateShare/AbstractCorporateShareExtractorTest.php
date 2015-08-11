133f6fddf9648030cf220b1227617c4f6f3a6ea6
ffsantos92 <ffsantos92@gmail.com>
2015-08-11 16:10:33 +0100

    Add abstract corporate share extractor test

---
+++ tests/Extractors/CorporateShare/AbstractCorporateShareExtractorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of Bens Penhorados, an undergraduate capstone project.
+ *
+ * (c) Fábio Santos <ffsantos92@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use App\Extractors\CorporateShareExtractorWrapper;
+
+/**
+ * This is the abstract corporate share extractor test class.
+ *
+ * @author Fábio Santos <ffsantos92@gmail.com>
+ */
+abstract class AbstractCorporateShareExtractorTest extends AbstractTestCase
+{
+    protected $extractor;
+
+    public function __construct()
+    {
+        $this->extractor = new CorporateShareExtractorWrapper();
+    }
+}
