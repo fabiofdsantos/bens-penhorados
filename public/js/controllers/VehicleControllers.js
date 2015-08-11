e814ffb5ca8d5a174d9d98ba15b00e67cc657c31
ffsantos92 <ffsantos92@gmail.com>
2015-08-11 23:29:55 +0100

    Working on filtering results

--- public/js/controllers/VehicleControllers.js
+++ public/js/controllers/VehicleControllers.js
@@ -2,21 +2,25 @@
 
 var app = angular.module('bens-penhorados');
 
-app.controller('VehicleListCtrl', ['$scope', 'Vehicle', function($scope, Vehicle) {
+app.controller('VehicleListCtrl', ['$scope', '$http', 'Vehicle', 'VehicleFilters', function($scope, $http, Vehicle, VehicleFilters) {
     $scope.items = [];
     $scope.totalItems = 0;
     $scope.itemsFrom = 0;
     $scope.itemsTo = 0;
     $scope.itemsPerPageValues = [5, 15, 25, 50, 100];
     $scope.itemsPerPage = 5;
+
+    getResultsPage(1);
+
     $scope.pagination = {
         current: 1
     };
 
-    getResultsPage(1);
+    getFilteringAttributes();
 
-    $scope.$watch('itemsPerPage', function(newValue, oldValue) {
-        if (!angular.equals(newValue, oldValue)) {
+    $scope.$watchGroup(['filterMake', 'filterModel', 'filterCategory', 'filterType'], function(newValues, oldValues, scope) {
+        if (!angular.equals(newValues, oldValues)) {
+            getFilteringAttributes();
             getResultsPage(1);
         }
     });
@@ -24,23 +28,34 @@ app.controller('VehicleListCtrl', ['$scope', 'Vehicle', function($scope, Vehicle
     function getResultsPage(pageNumber) {
         Vehicle.query({
             page: pageNumber,
-            limit: $scope.itemsPerPage
+            limit: $scope.itemsPerPage,
+            make: $scope.filterMake,
+            model: $scope.filterModel,
         }, function(data) {
             $scope.result = data;
             $scope.noResults = ((data.items.length) ? false : true);
         }, function(error) {});
     }
 
-    $scope.pageChanged = function(newPage) {
+    function getFilteringAttributes() {
+        VehicleFilters.query({
+            make: $scope.filterMake,
+        }, function(data) {
+            $scope.filteringAttr = data;
+        }, function(error) {});
+    }
+
+    $scope.pageChangeHandler = function(newPage) {
         getResultsPage(newPage);
     };
 }]);
 
-app.controller('VehicleCtrl', ['$scope', '$routeParams', '$location', 'Vehicle', function($scope, $routeParams, $location, Vehicle) {
+app.controller('VehicleCtrl', ['$rootScope', '$scope', '$routeParams', '$location', 'Vehicle', function($rootScope, $scope, $routeParams, $location, Vehicle) {
     Vehicle.get({
         slug: $routeParams.slug
     }, function(data) {
         $scope.vehicle = data;
+        $rootScope.pageTitle = data.title;
     }, function(error) {
         if (error.status === 404) {
             $location.path("/404");
