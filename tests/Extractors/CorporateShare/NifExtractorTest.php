778df981310da75d9bce5b94a240cf2782f04d2c
ffsantos92 <ffsantos92@gmail.com>
2015-08-11 16:10:49 +0100

    Add nif extractor test

---
+++ tests/Extractors/CorporateShare/NifExtractorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Bens Penhorados, an undergraduate capstone project.
+ *
+ * (c) Fábio Santos <ffsantos92@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * This is the nif extractor test class.
+ *
+ * @author Fábio Santos <ffsantos92@gmail.com>
+ */
+class NifExtractorTest extends AbstractCorporateShareExtractorTest
+{
+    public function testExtractNif()
+    {
+        $text = [
+            '100000000' => 100000000,
+            '200000000' => 200000000,
+            '500000000' => 500000000,
+            '600000000' => 600000000,
+            '800000000' => 800000000,
+            '900000000' => 900000000,
+        ];
+
+        foreach ($text as $input => $expected) {
+            $this->assertEquals($expected, $this->extractor->nif($input));
+        }
+    }
+}
