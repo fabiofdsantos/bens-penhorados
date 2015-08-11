883b34520e0d13c8cbc7d77e71cae21d7c25219c
ffsantos92 <ffsantos92@gmail.com>
2015-08-11 17:05:17 +0100

    Scope a query to only include fuels assigned at least to one vehicle

--- app/Models/Attributes/Vehicle/VehicleFuel.php
+++ app/Models/Attributes/Vehicle/VehicleFuel.php
@@ -11,6 +11,8 @@
 
 namespace App\Models\Attributes\Vehicle;
 
+use App\Models\Items\Vehicle;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -37,4 +39,18 @@ class VehicleFuel extends Model
      * @var string
      */
     protected $primaryKey = 'id';
+
+    /**
+     * Scope a query to only include fuels assigned at least to one vehicle.
+     *
+     * @param Builder $query
+     *
+     * @return Builder
+     */
+    public function scopeAssigned(Builder $query)
+    {
+        $fuels = Vehicle::distinct()->lists('fuel_id');
+
+        return $query->whereIn('id', $fuels)->orderBy('name');
+    }
 }
