84713639aa87badcb41dd1d845f736baa9d48f86
ffsantos92 <ffsantos92@gmail.com>
2015-08-12 21:37:26 +0100

    Create item_tax_offices table

---
+++ database/migrations/2015_00_00_000495_create_item_tax_offices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of Bens Penhorados, an undergraduate capstone project.
+ *
+ * (c) Fábio Santos <ffsantos92@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateItemTaxOfficesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('item_tax_offices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('code');
+
+            $table->integer('municipality_id')->unsigned();
+            $table->foreign('municipality_id')->references('id')->on('pt_municipalities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_tax_offices');
+    }
+}
