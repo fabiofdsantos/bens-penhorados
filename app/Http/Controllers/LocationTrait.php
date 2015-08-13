9e10e8146c6a0d3dab873e2a31b891a7d7f369cb
ffsantos92 <ffsantos92@gmail.com>
2015-08-13 01:54:12 +0100

    Add location trait

---
+++ app/Http/Controllers/LocationTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of Bens Penhorados, an undergraduate capstone project.
+ *
+ * (c) Fábio Santos <ffsantos92@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Http\Controllers;
+
+/**
+ * This is the location trait.
+ *
+ * @author Fábio Santos <ffsantos92@gmail.com>
+ */
+trait LocationTrait
+{
+    /**
+     * Get the location.
+     *
+     * @param \App\Models\Items\Item $generic
+     *
+     * @return string
+     */
+    public static function getLocation($generic)
+    {
+        $location = $generic->district()->pluck('name');
+        $location .= ', '.$generic->municipality()->pluck('name');
+
+        return $location;
+    }
+}
