79c1559f572519de374fc8b397f7c0587c9cb350
ffsantos92 <ffsantos92@gmail.com>
2015-08-14 12:57:39 +0100

    Optimize code

--- tests/Extractors/Vehicle/RegPlateCodeExtractorTest.php
+++ tests/Extractors/Vehicle/RegPlateCodeExtractorTest.php
@@ -18,52 +18,54 @@ class RegPlateCodeExtractorTest extends AbstractVehicleExtractorTest
 {
     public function testExtractValidGeneralFormat()
     {
-        $text = 'AA-99-99';
-        $this->assertSame('AA-99-99', $this->extractor->regPlateCode($text));
+        $text = [
+            'AA-99-99' => 'AA-99-99',
+            '99-AA-99' => '99-AA-99',
+            '99-99-AA' => '99-99-AA',
+        ];
 
-        $text = '99-AA-99';
-        $this->assertSame('99-AA-99', $this->extractor->regPlateCode($text));
-
-        $text = '99-99-AA';
-        $this->assertSame('99-99-AA', $this->extractor->regPlateCode($text));
+        foreach ($text as $input => $expected) {
+            $this->assertSame($expected, $this->extractor->regPlateCode($input));
+        }
     }
 
     public function testExtractValidTrailerFormat()
     {
-        $text = 'X-0';
-        $this->assertSame('X-0', $this->extractor->regPlateCode($text));
+        $text = [
+            'X-0'       => 'X-0',
+            'XX-0'      => 'XX-0',
+            'X-000000'  => 'X-000000',
+            'XX-000000' => 'XX-000000',
+        ];
 
-        $text = 'XX-0';
-        $this->assertSame('XX-0', $this->extractor->regPlateCode($text));
-
-        $text = 'X-000000';
-        $this->assertSame('X-000000', $this->extractor->regPlateCode($text));
-
-        $text = 'XX-000000';
-        $this->assertSame('XX-000000', $this->extractor->regPlateCode($text));
+        foreach ($text as $input => $expected) {
+            $this->assertSame($expected, $this->extractor->regPlateCode($input));
+        }
     }
 
     public function testExtractInvalidGeneralFormat()
     {
-        $text = 'AAA-00-00';
-        $this->assertNull($this->extractor->regPlateCode($text));
+        $text = [
+            'AAA-00-00',
+            'BB-000-00',
+            'CC-00-000',
+        ];
 
-        $text = 'BB-000-00';
-        $this->assertNull($this->extractor->regPlateCode($text));
-
-        $text = 'CC-00-000';
-        $this->assertNull($this->extractor->regPlateCode($text));
+        foreach ($text as $input => $expected) {
+            $this->assertNull($this->extractor->regPlateCode($input));
+        }
     }
 
     public function testExtractInvalidTrailerFormat()
     {
-        $text = 'A-1234567';
-        $this->assertNull($this->extractor->regPlateCode($text));
+        $text = [
+            'A-1234567',
+            'AA-1234567',
+            'AAA-123456',
+        ];
 
-        $text = 'AA-1234567';
-        $this->assertNull($this->extractor->regPlateCode($text));
-
-        $text = 'AAA-123456';
-        $this->assertNull($this->extractor->regPlateCode($text));
+        foreach ($text as $input => $expected) {
+            $this->assertNull($this->extractor->regPlateCode($input));
+        }
     }
 }
