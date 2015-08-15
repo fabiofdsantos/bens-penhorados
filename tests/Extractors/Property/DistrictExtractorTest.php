fa18a8e43169827c6703ae1af469ba0185711d73
ffsantos92 <ffsantos92@gmail.com>
2015-08-15 22:35:36 +0100

    Add the district extractor test

---
+++ tests/Extractors/Property/DistrictExtractorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Bens Penhorados, an undergraduate capstone project.
+ *
+ * (c) Fábio Santos <ffsantos92@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use App\Models\Attributes\Generic\District;
+
+/**
+ * This is the district extractor test class.
+ *
+ * @author Fábio Santos <ffsantos92@gmail.com>
+ */
+class DistrictExtractorTest extends AbstractPropertyExtractorTest
+{
+    public function testExtractDistrictName()
+    {
+        $text = [
+            'freguesia de Amora, Concelho de Seixal, Distrito de Setúbal' => 'Setúbal',
+        ];
+
+        foreach ($text as $input => $expected) {
+            $districtId = $this->extractor->district($input, null);
+
+            $this->assertNotNull($districtId);
+            $this->assertSame($expected, District::find($districtId)->name);
+        }
+    }
+}
