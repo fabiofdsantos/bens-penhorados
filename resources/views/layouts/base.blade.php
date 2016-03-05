62e493d35aa6dccd3fd60c0e085fcced46516f5c
Fabio Santos <ffsantos92@gmail.com>
2016-03-05 15:10:35 +0000

    Element nav does not need a role attribute

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -23,7 +23,7 @@
     <body>
         <!--[if lt IE 7]> <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p> <![endif]-->
 
-        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <nav class="navbar navbar-default navbar-fixed-top">
             @include('layouts.partials.nav')
         </nav>
             @yield('main')
