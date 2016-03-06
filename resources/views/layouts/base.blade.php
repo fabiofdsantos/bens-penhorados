9af56003f81ab1a8ed202d2994f53008ae815439
Fabio Santos <ffsantos92@gmail.com>
2016-03-06 22:23:41 +0000

    Use angularjs min version

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -43,7 +43,7 @@
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
 
         @yield('js')
     </body>
