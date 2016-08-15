5db35428d515e07c280ef6b0d9ab2c4d38aa9102
Fábio Santos <ffsantos92@gmail.com>
2016-08-15 01:17:40 +0100

    Load adsbygoogle.js ASAP

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -18,6 +18,8 @@
 </head>
 
 <body>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
     <!--[if lt IE 7]> <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p> <![endif]-->
 
     <script>
