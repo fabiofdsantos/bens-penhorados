82664a99a9773b5bb3876d64a1cf160b2e5f35af
Fábio Santos <ffsantos92@gmail.com>
2016-08-15 01:14:40 +0100

    Load adsbygoogle.js ASAP

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -57,7 +57,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({
             google_ad_client: "ca-pub-1654487041005723",
