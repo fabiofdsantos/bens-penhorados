dab0c8168685267bcee0cc2b97836897b350efc0
Fábio Santos <ffsantos92@gmail.com>
2019-06-20 12:45:07 +0100

    Add contact page to footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,4 +1,5 @@
 <p>
-  <a href="https://blog.benspenhorados.pt">Blogue</a> - 
-  <a href="/politica-de-privacidade">Política de Privacidade</a>
+  <a href="https://blog.benspenhorados.pt">Blogue</a> -
+  <a href="/politica-de-privacidade">Política de Privacidade</a> -
+  <a href="/contacto">Contacto</a>
 </p>
